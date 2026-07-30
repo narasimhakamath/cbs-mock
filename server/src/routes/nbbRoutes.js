aa9d767986db4ceed8205e76c9dba676733857f5
Narasimha Kamath <narasimha@datanimbus.com>
2026-07-30 13:13:12 +0530

    - Added CPR Sum API.

--- server/src/routes/nbbRoutes.js
+++ server/src/routes/nbbRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { depositAccountDetailsEnquiry } from '../controllers/nbbController.js';
 
 const router = Router();
+
+router.post('/CPRSum', depositAccountDetailsEnquiry);
 
 export default router;
