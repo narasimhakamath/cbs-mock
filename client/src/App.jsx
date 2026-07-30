40c46ae4eb97da9c5e270304309e58b380a4bffb
Narasimha Kamath <narasimha@datanimbus.com>
2026-07-30 11:14:03 +0530

    - Updated files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import AccountForm from './pages/AccountForm';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Navigate to="/parties" replace />} />
